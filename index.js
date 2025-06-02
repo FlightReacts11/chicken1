1939e55397109ff45bb82abfb06f9b74f5cf4e23
Gabriel <GCOverton@outlook.com>
2025-06-02 17:57:24 -0400

    commit 3

---
+++ index.js
@@ -0,0 +1,59 @@
+$(document).ready(() => {
+
+    $('.header-li.one').on('click', () => {
+        $('.food-menu').toggleClass('hide')
+    });
+
+    $('.header-li.one').on('click', () => {
+        $('.food-menu').fadeToggle(330)
+        })
+        
+
+
+})
+
+
+$(document).ready(function() {
+    $(".combo-box").each(function() {
+      let box = $(this);
+      let counter = box.find(".counter");
+      let count = parseInt(counter.text());
+      const limit = 10;
+
+      box.find(".add-item").click(function() {
+        if (count < limit) {
+        count++;
+        counter.text(count);
+        }
+      });
+  
+      box.find(".subtract-item").click(function() {
+        if (count > 0) {
+          count--;
+          counter.text(count);}
+      });
+    });
+  });
+
+
+  $(document).ready(function() {
+    let current = 0;
+    const $slides = $('.slide');
+    const allSlides = $slides.length;
+
+    function showSlide(index) {
+       $slides.removeClass('active');
+       $slides.eq(index).addClass('active');
+    }
+
+    $('.java-arrow-left').click(function () {
+      current = (current - 1 + allSlides) % allSlides;
+      showSlide(current);
+    });
+
+    $('.java-arrow-right').click(function () {
+      current = (current + 1) % allSlides;
+      showSlide(current);
+    });
+
+  });
